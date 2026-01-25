8aa793053ef952f6c0871f2d8341557940aa96ca
Rebeca F Matias <rebecafeitosamatias@gmail.com>
2026-01-25 18:15:10 -0300

    creating docker-compose file

--- dags/etlweather.py
+++ dags/etlweather.py
@@ -44,6 +44,57 @@ with DAG(dag_id="weather_etl_pipeline",
         else:
             logging.error(f"Failed to fetch weather data: {response.status_code}")
 
-    # @task()
-    # def transform_weather_data(weather_data):
+    @task()
+    def transform_weather_data(weather_data):
+        current_weather = weather_data['current_weather']
+        transformed_data = {
+            'latitude': latitude,
+            'longitude': longitude,
+            'temperature': weather_data['temperature'],
+            'windspeed': weather_data['windspeed'],
+            'winddirection': weather_data['winddirection'],
+            'weathercode': weather_data['weathercode']
+        }
+        return transform_weather_data
+    
+    @task()
+    def load_weather_data(transformed_data):
+        pg_hook = PostgresHook(postgres_conn_id=postgres_conn_id)
+        conn = pg_hook.get_conn()
+        cursor = conn.cursor()
 
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS weather_data (
+            latitude FLOAT,
+            longitude FLOAT,
+            temperature FLOAT,
+            windspeed FLOAT,
+            winddirection FLOAT,
+            weathercode INT,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        """)
+
+        cursor.execute("""
+        INSERT INTO weather_data (
+            latitude,
+            longitude,
+            temperature,
+            windspeed,
+            winddirection,
+            weathercode
+            )
+        VALUES (%s, %s, %s, %s, %s, %s)
+        """, (
+            transformed_data['latitude'],
+            transformed_data['longitude'],
+            transformed_data['temperature'],
+            transformed_data['windspeed'],
+            transformed_data['winddirection'],
+            transformed_data['weathercode']
+        ))
+
+    ## DAG Workflow - ETL Pipeline
+    weather_data = extract_weather_data()
+    transformed_data = transform_weather_data(weather_data)
+    load_weather_data(transformed_data)
